e55a6e451678b0d54149637633ed76a2a6c6590b
Evelina Kutalo <kutaloea@gmail.com>
2023-09-04 21:38:26 +0300

    chore: Update cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: "t64w2f",
   env: {
-    baseUrl: 'http://dev.listalpha.com',
+    baseUrl: 'https://dev-ui.listalpha.com/',
   },
   e2e: {
-    baseUrl: 'http://dev.listalpha.com',
+    baseUrl: 'https://dev-ui.listalpha.com/',
 
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
 
     viewportWidth: 1440,
     viewportHeight: 970,
     videoUploadOnPasses: false,
-    screenshotOnRunFailure: false,
     retries: 0,
-    video: false,
+    video: true,
+    videoCompression: 32,
     requestTimeout: 20000,
     defaultCommandTimeout: 20000,
   },

bfe8937c635f1c93784f4f98ba2b948b7aaba25f
Evelina Kutalo <kutaloea@gmail.com>
2023-10-06 18:55:46 +0300

    fix: signIn with 0auth

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,29 @@
-import 'cypress-wait-until';
+import "cypress-wait-until";
 
-Cypress.Commands.add(
-    'signInUser',
-    () => {
-        cy.visit("https://dev-ui.listalpha.com")
+function loginViaAuth0Ui(username, password) {
+  // App landing page redirects to Auth0.
+  cy.visit("/");
 
-        cy.get('form').first().within(() => {
-            cy.get("#username").type("e.kutalo@geniusee.com")
-
-            cy.get("#password").type("test123TEST")
-
-            cy.get("button[type='submit']").filter(":visible").click()
-        })
-    },
+  // Login on Auth0.
+  cy.origin(
+    "https://listalpha-development.eu.auth0.com",
+    { args: { username, password } },
+    ({ username, password }) => {
+      cy.get("form")
+        .first()
+        .within(() => {
+          cy.get("#username").type(username);
+          cy.get("#password").type(password);
+          cy.get("button[type='submit']").filter(":visible").click();
+        });
+    }
   );
+
+  // Ensure Auth0 has redirected us back to the RWA.
+  // cy.url().should('equal', 'https://dev-ui.listalpha.com')
+}
+
+Cypress.Commands.add("signInUser", () => {
+  loginViaAuth0Ui("e.kutalo@geniusee.com", "test123TEST");
+  cy.wait(2000);
+});
